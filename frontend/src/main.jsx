088eeec7835bf6a4dc6caaa51b9bb5d4ffd4fd56
sasiya453 <sasindulasith00@gmail.com>
2025-08-16 10:21:28 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+- import { BrowserRouter } from "react-router-dom";
++ import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+-   <BrowserRouter>
++   <HashRouter>
       <App />
-    </BrowserRouter>
+-   </BrowserRouter>
++   </HashRouter>
   </React.StrictMode>
 );
